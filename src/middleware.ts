c72b7feee06229a9ef5a1f2f8420314356bea69d
waleeddevg <waleedgopang1@gmail.com>
2026-05-30 10:04:23 +0500

    fix: exclude sitemap and robots from Clerk middleware and update sitemap fallback

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|xml|txt)).*)',
     '/(api|trpc)(.*)',
   ],
 }
